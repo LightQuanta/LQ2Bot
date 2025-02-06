2074b14ecad06336c8af1a517d26c0eb22530464
Light_Quanta <zch22248@foxmail.com>
2025-02-07 03:03:14 +0800

    feat: 允许管理员或群主使用Mute借刀杀人

--- src/main/kotlin/tech/lq0/plugins/Mute.kt
+++ src/main/kotlin/tech/lq0/plugins/Mute.kt
@@ -1,5 +1,6 @@
 package tech.lq0.plugins
 
+import love.forte.simbot.component.onebot.v11.core.actor.OneBotMember
 import love.forte.simbot.component.onebot.v11.core.actor.OneBotMemberRole
 import love.forte.simbot.component.onebot.v11.core.event.message.OneBotNormalGroupMessageEvent
 import love.forte.simbot.message.At
@@ -11,6 +12,7 @@ import love.forte.simbot.quantcat.common.annotations.FilterValue
 import love.forte.simbot.quantcat.common.annotations.Listener
 import org.springframework.stereotype.Component
 import tech.lq0.interceptor.FunctionSwitch
+import tech.lq0.utils.botPermissionConfig
 import kotlin.time.Duration.Companion.minutes
 
 @Component
@@ -30,28 +32,27 @@ class Mute {
     @Filter("禁言抽奖")
     suspend fun OneBotNormalGroupMessageEvent.randomMute() {
         val time = timeRange.random()
-        with(author()) {
-            // 换用@应该就不用审核用户名了吧（）
-            if (role != OneBotMemberRole.MEMBER) {
-                content().send(
-                    messagesOf(
-                        "恭喜 ".toText(),
-                        At(authorId),
-                        " 抽中了1145141919810分钟禁言套餐！".toText(),
-                    )
+        val target = getFirstAtOrAuthor() ?: return
+
+        // 换用@应该就不用审核用户名了吧（）
+        if (target.role != OneBotMemberRole.MEMBER) {
+            content().send(
+                messagesOf(
+                    "恭喜 ".toText(),
+                    At(authorId),
+                    " 抽中了1145141919810分钟禁言套餐！".toText(),
                 )
-            } else {
-                ban(time.minutes)
-                content().send(
-                    messagesOf(
-                        "恭喜 ".toText(),
-                        At(authorId),
-                        " 抽中了${time}分钟禁言套餐！".toText(),
-                    )
+            )
+        } else {
+            target.ban(time.minutes)
+            content().send(
+                messagesOf(
+                    "恭喜 ".toText(),
+                    At(authorId),
+                    " 抽中了${time}分钟禁言套餐！".toText(),
                 )
-            }
+            )
         }
-        // TODO 爬行者榜？
     }
 
     @Listener
@@ -59,22 +60,36 @@ class Mute {
     @ContentTrim
     @Filter("自助禁言 {{time,\\d{1,10}}}")
     suspend fun OneBotNormalGroupMessageEvent.customMute(@FilterValue("time") time: Int) {
-        with(author()) {
-            if (role != OneBotMemberRole.MEMBER) {
-                content().send("在？有种把管理卸了")
-            } else {
-                val processedTime = time.coerceIn(1..720)
-                ban(processedTime.minutes)
-                // 换用@应该就不用审核用户名了吧（）
-                content().send(
-                    messagesOf(
-                        "恭喜 ".toText(),
-                        At(authorId),
-                        " 成功领取了${processedTime}分钟的禁言套餐！".toText(),
-                    )
+        val target = getFirstAtOrAuthor() ?: return
+
+        if (target.role != OneBotMemberRole.MEMBER) {
+            content().send("在？有种把管理卸了")
+        } else {
+            val processedTime = time.coerceIn(1..720)
+            target.ban(processedTime.minutes)
+            // 换用@应该就不用审核用户名了吧（）
+            content().send(
+                messagesOf(
+                    "恭喜 ".toText(),
+                    At(authorId),
+                    " 成功领取了${processedTime}分钟的禁言套餐！".toText(),
                 )
-            }
+            )
         }
+    }
+
+    suspend fun OneBotNormalGroupMessageEvent.getFirstAtOrAuthor(): OneBotMember? {
+        val messages = messageContent.messages.toList()
+        val id = if (
+            (author().role != OneBotMemberRole.MEMBER || authorId.toString() in botPermissionConfig.admin)
+            && messages.filterIsInstance<At>().isNotEmpty()
+        ) {
+            // 允许管理员或群主借刀杀人
+            messages.filterIsInstance<At>().first().target
+        } else {
+            return author()
+        }
+        return this.content().member(id)
     }
 
 }
