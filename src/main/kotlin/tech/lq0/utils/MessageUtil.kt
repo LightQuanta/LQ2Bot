a02401eebf90336a0b54e2b5ff7bc83b9cc0fc34
Light_Quanta <zch22248@foxmail.com>
2026-02-02 13:54:02 +0800

    refactor: 将定时撤回修改为聊天记录转发

--- src/main/kotlin/tech/lq0/utils/MessageUtil.kt
+++ src/main/kotlin/tech/lq0/utils/MessageUtil.kt
@@ -1,15 +1,15 @@
 package tech.lq0.utils
 
-import kotlinx.coroutines.*
 import love.forte.simbot.component.onebot.v11.core.event.message.OneBotFriendMessageEvent
 import love.forte.simbot.component.onebot.v11.core.event.message.OneBotGroupMessageEvent
 import love.forte.simbot.component.onebot.v11.core.event.message.OneBotGroupPrivateMessageEvent
 import love.forte.simbot.component.onebot.v11.core.event.message.OneBotMessageEvent
-import love.forte.simbot.component.onebot.v11.message.OneBotMessageReceipt
+import love.forte.simbot.component.onebot.v11.message.segment.OneBotAt
+import love.forte.simbot.component.onebot.v11.message.segment.OneBotForwardNode
 import love.forte.simbot.component.onebot.v11.message.segment.OneBotText
+import love.forte.simbot.component.onebot.v11.message.segment.toElement
 import love.forte.simbot.message.*
 import tech.lq0.interceptor.addMemberRateLimit
-import kotlin.time.Duration.Companion.seconds
 
 /**
  * 直接发送消息，不会额外创建一条回复
@@ -25,14 +25,14 @@ const val LONG_MESSAGE_LENGTH = 500
 /**
  * 直接发送消息，不会额外创建一条回复
  */
-suspend fun OneBotMessageEvent.directlySend(messages: Messages, autoRevoke: Boolean = false) {
+suspend fun OneBotMessageEvent.directlySend(messages: Messages, foldLongMessage: Boolean = false) {
     // 防止意外响应黑名单成员
     if (authorId.toString() in botPermissionConfig.memberBlackList) return
     // 为用户添加功能调用限流
     addMemberRateLimit(authorId.toString())
 
-    // 是否在60s后自动撤回过长消息
-    val shouldRevoke = autoRevoke && messages.toText().length > LONG_MESSAGE_LENGTH
+    // 是否将过长消息修改为转发消息
+    val foldLongMessage = foldLongMessage && messages.toText().length > LONG_MESSAGE_LENGTH
 
     when (this) {
         is OneBotGroupMessageEvent -> {
@@ -42,7 +42,12 @@ suspend fun OneBotMessageEvent.directlySend(messages: Messages, autoRevoke: Bool
                 || content.id.toString() in botPermissionConfig.groupBlackList
             ) return
 
-            content.send(messages).autoRevoke(shouldRevoke)
+            if (foldLongMessage) {
+                content.send(OneBotForwardNode.create(bot.id, bot.name, messages.toOneBotMessage()).toElement())
+            } else {
+                content.send(messages)
+            }
+
             chatLogger.info("bot <- 群 ${content.name}(${content.id}): ${messages.toText()}")
         }
 
@@ -54,38 +59,31 @@ suspend fun OneBotMessageEvent.directlySend(messages: Messages, autoRevoke: Bool
             ) return
 
             val content = content()
-            reply(messages)
+            if (foldLongMessage) {
+                reply(OneBotForwardNode.create(bot.id, bot.name, messages.toOneBotMessage()).toElement())
+            } else {
+                reply(messages)
+            }
             chatLogger.info("bot <- 群 ${content.name}(${content.id}) ${content().nick ?: content().name}($authorId): ${messages.toText()}")
         }
 
         is OneBotFriendMessageEvent -> {
             val content = content()
-            reply(messages)
+            if (foldLongMessage) {
+                reply(OneBotForwardNode.create(bot.id, bot.name, messages.toOneBotMessage()).toElement())
+            } else {
+                reply(messages)
+            }
             chatLogger.info("bot <- ${content.name}(${content.id}): ${messages.toText()}")
         }
 
         else -> {
-            reply(messages).autoRevoke(shouldRevoke)
+            if (foldLongMessage) {
+                reply(OneBotForwardNode.create(bot.id, bot.name, messages.toOneBotMessage()).toElement())
+            } else {
+                reply(messages)
+            }
             chatLogger.info("bot <- ? : ${messages.toText()}")
-        }
-    }
-}
-
-val autoRevokeScope = CoroutineScope(Dispatchers.Default + SupervisorJob())
-
-/**
- * 在60s后自动撤回已发送消息
- * @param revoke 是否启用自动撤回
- */
-private fun OneBotMessageReceipt.autoRevoke(revoke: Boolean) {
-    if (!revoke) return
-
-    autoRevokeScope.launch {
-        delay(60.seconds)
-        try {
-            delete()
-        } catch (e: Exception) {
-            chatLogger.error("自动撤回失败: $e")
         }
     }
 }
@@ -151,3 +149,17 @@ fun Messages.toText() = toList().joinToString("") {
  * 将消息转换为文本
  */
 fun MessageContent.toText() = messages.toText()
+
+/**
+ * 将消息转换为List<OneBotMessage>
+ */
+fun Messages.toOneBotMessage() = toList().map {
+    when (it) {
+        is At -> OneBotAt.create(it.target)
+        is Text -> OneBotText.create(it.text)
+        is OneBotText.Element -> OneBotText.create(it.text)
+
+        // 有必要支持其他类型吗）
+        else -> OneBotText.create("")
+    }
+}
